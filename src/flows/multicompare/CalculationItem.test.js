b3044d0a688770285228b9c863dd5182cc018b32
Daniel Pinson <dpinson@captechconsulting.com>
2020-03-09 13:17:31 -0400

    Bug fix for endless loop when calculating score

---
+++ src/flows/multicompare/CalculationItem.test.js
@@ -0,0 +1,16 @@
+import {fireEvent, render} from '@testing-library/react'
+import CalculationItem from "./CalculationItem";
+import React from "react";
+
+it('calculation item should calculate score on input change', () => {
+    const { getByPlaceholderText, getByText } = render(<CalculationItem showScore={true}/>);
+    const volumeInput = getByPlaceholderText('Volume');
+    const abvInput = getByPlaceholderText('% APV');
+    const beerInput = getByPlaceholderText('Beer Name');
+    const costInput = getByPlaceholderText('Cost');
+    fireEvent.change(beerInput, {target: {value: 'Bud Light'}});
+    fireEvent.change(abvInput, {target: {value: '5'}});
+    fireEvent.change(costInput, {target: {value: '6'}});
+    fireEvent.change(volumeInput, {target: {value: '12'}});
+    getByText('Ottaway Score: 0.78');
+});
